59366941077f54b3703e465ca2b3521478c832d2
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2024-03-25 10:29:19 +0530

    Add files via upload

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,74 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+const fullScreenBtn = document.getElementById("full-btn");
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function jumpingToSpecificTime(e) {
+  const newTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = newTime;
+}
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
+
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+skipButtons.forEach((btn) => btn.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", jumpingToSpecificTime);
+progress.addEventListener(
+  "mousemove",
+  (e) => mousedown && jumpingToSpecificTime(e)
+);
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
+
+fullScreenBtn.addEventListener("click", () => {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    /* Firefox */
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    /* IE/Edge */
+    video.msRequestFullscreen();
+  }
+});
